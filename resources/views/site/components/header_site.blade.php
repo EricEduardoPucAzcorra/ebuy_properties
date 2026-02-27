db210685fb36aa24ec333634fab7028d88f6b2b8
eric.azcorra.24 <eric.azcorra.24@gmail.com>
2026-02-27 09:57:05 -0600

    prueba etxt

--- resources/views/site/components/header_site.blade.php
+++ resources/views/site/components/header_site.blade.php
@@ -17,8 +17,10 @@
 
                     <p class="ebuy-hero-subtitle animated fadeIn mb-4">
                         <p class="ebuy-hero-subtitle animated fadeIn mb-4">
-                            {{ auto_trans('Encuentra un hogar diseñado para brindar comodidad, seguridad y bienestar.') }}
-                            {{ auto_trans('Un espacio donde cada momento se convierte en un recuerdo especial.') }}
+                            {{ auto_trans('Encuentra un hogar diseñado para brindar') }}
+                            {{ auto_trans('comodidad, seguridad y bienestar.') }}
+                            {{ auto_trans('Un espacio donde cada momento') }}
+                            {{ auto_trans('se convierte en un recuerdo especial.') }}
                         </p>
                     </p>
 
